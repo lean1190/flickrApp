d93061800edbf450d4ce27d7f40a086233977c78
leito9008 <leito.vm3@hotmail.com>
2016-08-21 16:08:14 -0300

    First commit, project structure generated with yeoman's angular-generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flickrAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the flickrAppApp
+ */
+angular.module('flickrAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
