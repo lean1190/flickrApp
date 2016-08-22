3137942531f2f34fd0978e1230f51b4f12d221de
leito9008 <leito.vm3@hotmail.com>
2016-08-21 21:21:10 -0300

    Added some custom validation for tags

--- app/scripts/main/main.module.js
+++ app/scripts/main/main.module.js
@@ -8,4 +8,20 @@
  *
  * Module for setting the main application structure and common behavior
  */
-angular.module('flickrApp.main', []);
+angular
+    .module('flickrApp.main', [])
+    .config(function ($validationProvider) {
+        
+        /* Setup a custom tags validation expression */
+        var tagsExpression = {
+            tags: /^\w(\s*,?\s*\w)*$/
+        },
+        validMsg = {
+            tags: {
+                error: 'This are not tags',
+                success: 'Tags okey'
+            }
+        };
+
+        $validationProvider.setExpression(tagsExpression).setDefaultMsg(validMsg);
+    });
